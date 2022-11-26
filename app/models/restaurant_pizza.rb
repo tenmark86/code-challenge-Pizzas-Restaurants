336c0cf787e68aad0c85c653a1cad0cc28795ab8
tenmark86e <tenmark86@gmail.com>
2022-11-26 14:50:05 +0300

    added README.md file

--- app/models/restaurant_pizza.rb
+++
@@ -1,6 +0,0 @@
-class RestaurantPizza < ApplicationRecord
-    validates :price, numericality: { in: 1..30 }
-
-    belongs_to :pizza
-    belongs_to :restaurant
-end
--- app/serializers/restaurant_pizza_serializer.rb
+++
@@ -1,4 +0,0 @@
-class RestaurantPizzaSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address
-  has_many :pizzas
-end
